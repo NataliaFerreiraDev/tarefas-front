5031cbbb6cdfe4b511ae672773cf8cba92908fd6
Natalia Ferreira <nataliaferreira.dev@yahoo.com>
2025-03-22 21:13:41 -0300

    Adiciona os componentes Footer e Toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ToolbarComponent } from "./layout/toolbar/toolbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FooterComponent,
+    ToolbarComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    RouterModule
+  ]
+})
+export class LayoutModule { }
